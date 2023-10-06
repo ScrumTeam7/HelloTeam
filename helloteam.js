7881c7288e7eae7c1ef8a650a2d59fa6089ca8c2
Glynax <glynax@gmail.com>
2023-10-06 13:51:11 -0400

    Foreach loop

--- helloteam.js
+++ helloteam.js
@@ -1,4 +1,4 @@
 const teamNames = ["Matthew Brown", "Sheldon Uchytil", "Rafael Jimenez", "Evans Odeh"];
-for (let i = 0; i < teamNames.length; i++) {
-    console.log(`Hello ${teamNames[i]}`);
-  }
+teamNames.forEach(name => {
+  console.log(`Hello ${name}`);
+});
